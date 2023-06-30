941ae231615949794b4098f9b06d78ddbd97ad8a
Melvyn <melvynhoarau@hotmail.fr>
2023-06-30 08:51:33 +0800

    Desploy

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,194 @@
+'use client'
 import Image from 'next/image'
+import {BsFillMoonStarsFill, BsFillSunFill, BsGithub, BsTelegram, BsMessenger} from 'react-icons/bs'
+import deved from "../../public/dev-ed-wave.png"
+import design from "../../public/design.png"
+import code from "../../public/code.png"
+import consulting from "../../public/consulting.png"
+import web1 from "../../public/web1.png"
+import web2 from "../../public/web2.png"
+import web3 from "../../public/web3.png"
+import web4 from "../../public/web4.png"
+import web5 from "../../public/web5.png"
+import web6 from "../../public/web6.png"
+import { useState } from "react"
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleMoonClick = () => {
+    setDarkMode(!darkMode);
+  };
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_ok34pd9', 'template_yr9yz4q', form.current, 'pGbqVmTvTEyeeMMts')
+        .then((result) => {
+            alert("Email sent !");
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        e.target.reset()
+      };
+
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={darkMode ? "dark" : ""}>
+      <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-burtons dark:text-white dark:hover:text-teal-500 hover:text-teal-500 hover:scale-110 transition duration-500'><a href="#contact">Contact me</a></h1>
+            <ul className='flex items-center'>
+              <li>
+              {darkMode ? (
+              <BsFillSunFill onClick={handleMoonClick} className='cursor-pointer text-2xl hover:rotate-90 transition duration-1000 dark:text-white'/>
+            ) : (
+              <BsFillMoonStarsFill onClick={handleMoonClick} className='cursor-pointer text-2xl hover:-rotate-90 transition duration-1000 dark:text-white'/>
+            )}
+              </li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8 hover:from-cyan-400 hover:to-teal-400' href="#">Download CV</a></li>
+            </ul>
+          </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Melvyn Hoarau</h2>
+            <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Developer and designer.</h3>
+            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-200'>
+              Freelancer providing services for programming and design content needs. Join me down below and let's get your idea to reality!
+            </p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400'>
+            <a href="https://github.com/0xMelvyn?tab=repositories" target='#blank'><BsGithub className='hover:text-black dark:hover:text-white transition duration-500' /></a>
+            <a href="https://twitter.com/TheDevIsCooking" target='#blank'><BsTelegram className='hover:text-black dark:hover:text-white transition duration-500' /></a>
+            <a href=""><BsMessenger className='hover:text-black dark:hover:text-white transition duration-500' /></a>
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden my-10 md:h-96 md:w-96'>
+            <Image className='hover:scale-110 transition duration-500' src={deved} layout="fill" objectFit="cover" />
+          </div>
+        </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section>
+          <div>
+            <h3 className='text-3xl py-1 mt- dark:text-white'>Services I Offer</h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              Since the beginning of my journey as a freelance designer and developer, I've done remote work for
+              <span className='text-teal-500'> agencies </span>
+              consulted for <span className='text-teal-500'>starups </span>
+              and collabored with talanted people to create digital products for both business and consumer use.
+            </p>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              I offer from a wide range of services, including brand design, programming and teaching.
+            </p>
+          </div>
+          <div className='lg:flex gap-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 flex-1 hover:scale-110 transition'>
+              <Image className='mx-auto' src={design} width={100} height={100} flex-1/>
+              <h3 className='text-lg font-medium pt-8 pb-2 dark:text-white'>Beautiful Designs</h3>
+              <p className='py-2 dark:text-white'>
+                Creating modern and aesthetic designs suited for you, following your design theory.
+              </p>
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>Gimp</p>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>Canva</p>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>A.I</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 flex-1 hover:scale-110 transition'>
+              <Image className='mx-auto' src={code} width={100} height={100} flex-1/>
+              <h3 className='text-lg font-medium pt-8 pb-2 dark:text-white'>Clean Code</h3>
+              <p className='py-2 dark:text-white'>
+                Programming multi-device, fast and secure website for a smooth experience and SEO.
+              </p>
+              <h4 className='py-4 text-teal-600'>Stack I know</h4>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>HTML/CSS/JS</p>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>Tailwind/React/Next</p>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>PHP/SQL</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 flex-1 hover:scale-110 transition'>
+              <Image className='mx-auto' src={consulting} width={100} height={100} flex-1/>
+              <h3 className='text-lg font-medium pt-8 pb-2 dark:text-white'>Always Available</h3>
+              <p className='py-2 dark:text-white'>
+                No worries if you need an update, modify your website or start a new project, we keep contact.
+              </p>
+              <h4 className='py-4 text-teal-600'>Let's Talk</h4>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>Telegram</p>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>Messenger</p>
+              <p className='text-gray-800 py-1 dark:text-gray-200'>Email</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className='text-3xl py-1 dark:text-white'>Portfolio</h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              Since the beginning of my journey as a freelance designer and developer, I've done remote work for
+              <span className='text-teal-500'> agencies </span>
+              consulted for <span className='text-teal-500'>starups </span>
+              and collabored with talanted people to create digital products for both business and consumer use.
+            </p>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              I offer from a wide range of services, including brand design, programming and teaching.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+              <div className="basis-1/3 flex-1 ">
+                <a href="https://0xmelvyn.github.io/ecommerce/" target='#blank'><Image className="rounded-lg object-cover hover:scale-110 transition duration-500" width={"100%"} height={"100%"} layout="responsive" src={web1}/></a>
+              </div>
+              <div className="basis-1/3 flex-1 ">
+                <a href="https://php-blog-portfolio.000webhostapp.com/" target='#blank'><Image className="rounded-lg object-cover hover:scale-110 transition duration-500" width={"100%"} height={"100%"} layout="responsive" src={web2}/></a>
+              </div>
+            </div>
+            <section id="contact">
+              <div>
+              <h3 className='text-3xl py-1 mt- dark:text-white'>Contact Me</h3>
+              <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              Feel free to write me a mail with this form, if you have any
+              <span className='text-teal-500'> question </span>
+              or to <span className='text-teal-500'>contact </span>
+              me. I'll answer as fast as I can !
+            </p>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
+              You can also add me on my social network.
+            </p>
+            <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+              <div className='flex flex-col text-center dark:text-white basis-1/8 flex-1 gap-10 leading-7'>
+                    <article className="flex flex-col bg-teal-600 rounded-lg py-5">
+                        <BsGithub className='mx-auto text-3xl text-white'/>
+                        <h4 className='text-white'>Email</h4>
+                        <h5 className='text-sm text-gray-300'>melvynhoarau@icloud.com</h5>
+                        <a href="mailto:melvynhoarau@icloud.com" target='_blank'>Send a message</a>
+                    </article>
+                    <article className="flex flex-col bg-teal-600 rounded-lg py-5">
+                        <BsMessenger className='mx-auto text-3xl text-white'/>
+                        <h4 className='text-white'>Messenger</h4>
+                        <h5 className='text-sm text-gray-300'>Melvyn Hoarau</h5>
+                        <a href="https://www.messenger.com/t/100088349492050/" target='_blank'>Send a message</a>
+                    </article>
+                    <article className="flex flex-col bg-teal-600 rounded-lg py-5">
+                        <BsTelegram className='mx-auto text-3xl text-white'/>
+                        <h4 className='text-white'>Telegram</h4>
+                        <h5 className='text-sm text-gray-300'>@MelvynHoarau</h5>
+                        <a href="https://t.me/MelvynHoarau" target='_blank'>Send a message</a>
+                    </article>
+              </div>
+              <div className='flex flex-col text-center basis-1/3 flex-1'>
+                  <form className='flex flex-col gap-5 py-5' ref={form} onSubmit={sendEmail}>
+                      <input className='shadow-lg border-2 border-solid border-teal-500 bg-transparent rounded-lg p-5 dark:text-white' type="text" name='name' placeholder='Your full Name' required/>
+                      <input className='shadow-lg border-2 border-solid border-teal-500 bg-transparent rounded-lg p-5 dark:text-white' type="email" name='email' placeholder='Your Email' required/>
+                      <textarea className='shadow-lg border-2 border-solid border-teal-500 bg-transparent rounded-lg p-5 dark:text-white' name="message" rows="7" placeholder='Your Message' required></textarea>
+                      <button className='shadow-lg border-2 border-solid border-teal-500 bg-teal-400 rounded-lg p-5' type='submit'>Send Message</button>
+                  </form>
+              </div>
+              </div>
+            </div>
+            </section>
+        </section>
+      </main>
+    </div>
   )
 }
